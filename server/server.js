e5d4b76e925363393213f906996d7ff8a6037d3d
HIMANSHU <hs764664@gmail.com>
2026-02-04 21:18:22 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -46,13 +46,14 @@ app.use('/api/messages', messageRouter);
 const PORT = process.env.PORT || 5000;
 
 // DB Connect hone ke baad server start karein
-const startServer = async () => {
-    try {
-        await connectDB();
-        server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-    } catch (error) {
-        console.log("Server error:", error);
-    }
-}
 
-startServer();
+
+await connectDB()
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT,  () => {
+    
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+export default server;
